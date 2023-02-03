3cac05e854af8b27dd2850c34958005df98fb728
jasper.lai <jasper.lai@gmail.com>
2023-02-03 18:07:32 +0800

    加入一些 預期行為:的描述, 但不知描述是否正確?

--- NETCoreDI/Program.cs
+++ NETCoreDI/Program.cs
@@ -100,48 +100,63 @@ namespace NETCoreDI
             serviceProvider1 = serviceCollection.BuildServiceProvider();
 
             #region 使用預設 Scope
-            sayHello1 = serviceProvider1.GetService<ISayHello>();
-            sayHello1.Hi("M1 - Will");
-            sayHello2 = serviceProvider1.GetService<ISayHello>();
-            sayHello2.Hi("M2 - Lee");
-            sayHello1 = null;
-            sayHello2 = null;
-            GC.Collect(2);
-            Thread.Sleep(1000);
-            sayHello3 = serviceProvider1.GetService<ISayHello>();
-            sayHello3.Hi("M9 - Will");
-            #endregion
-
-            #region 使用兩個 Scope
-
-            //serviceScope2 = serviceProvider1.CreateScope();
-            //serviceProvider2 = serviceScope2.ServiceProvider;
-            //sayHello1 = serviceProvider2.GetService<ISayHello>();
+            //sayHello1 = serviceProvider1.GetService<ISayHello>();
             //sayHello1.Hi("M1 - Will");
-            //sayHello2 = serviceProvider2.GetService<ISayHello>();
+            //sayHello2 = serviceProvider1.GetService<ISayHello>();
             //sayHello2.Hi("M2 - Lee");
             //sayHello1 = null;
             //sayHello2 = null;
             //GC.Collect(2);
             //Thread.Sleep(1000);
-            //sayHello3 = serviceProvider2.GetService<ISayHello>();
-            //sayHello3.Hi("M9 - Will");
+            //sayHello3 = serviceProvider1.GetService<ISayHello>();
+            //sayHello3.Hi("M3 - Will");
+            #endregion
 
-            //serviceScope3 = serviceProvider1.CreateScope();
-            //serviceProvider3 = serviceScope3.ServiceProvider;
-            //sayHello1_1 = serviceProvider3.GetService<ISayHello>();
-            //sayHello1_1.Hi("M1_1 - Ada");
-            //sayHello2_1 = serviceProvider3.GetService<ISayHello>();
-            //sayHello2_1.Hi("M2_1 - Chan");
-            //sayHello1_1 = null;
-            //sayHello2_1 = null;
-            //GC.Collect(2);
-            //Thread.Sleep(1000);
-            //sayHello3_1 = serviceProvider3.GetService<ISayHello>();
-            //sayHello3_1.Hi("M3_1 - Ada Chan");
-            //// 若將底下的程式碼註解起來(在 AddScoped 模式)，則 
-            //// sayHello1, sayHello2 指向到 ConsoleMessage 會被釋放掉
-            ////sayHello3.Hi("M3 - Will");
+            // -----------------------------------------------------
+            // (jasper)
+            // 1.. 因為是 AddScoped, 建立不同的 scope 的狀況下, 會有不同的物件實體
+            // 2.. sayHello1, sayHello2 被設成 null, 因為還在 scope 中, 所以不會被釋放
+            // 3.. sayHello1_1, sayHello2_1 被設成 null, 因為還在 scope 中, 所以不會被釋放;
+            //     但此時因為前一個 scope 已經結束, 所以 sayHello1 (與 sayHello2 相同 instance), 
+            //     應該要被解構掉.
+            //     --> 但實測結果是完全沒有被釋放, 不論 sayHello3.Hi("M3 - Will"); 是否有被註解掉
+            // -----------------------------------------------------
+
+            #region 使用兩個 Scope
+
+            // ----------------- scope2 ------------------------
+            serviceScope2 = serviceProvider1.CreateScope();
+            serviceProvider2 = serviceScope2.ServiceProvider;
+            sayHello1 = serviceProvider2.GetService<ISayHello>();
+            sayHello1.Hi("M1 - Will");
+            sayHello2 = serviceProvider2.GetService<ISayHello>();
+            sayHello2.Hi("M2 - Lee");
+            sayHello1 = null;
+            sayHello2 = null;
+            GC.Collect(2);
+            Thread.Sleep(1000);
+            sayHello3 = serviceProvider2.GetService<ISayHello>();
+            sayHello3.Hi("M3 - Will");
+
+            // ----------------- scope3 ------------------------
+            serviceScope3 = serviceProvider1.CreateScope();
+            serviceProvider3 = serviceScope3.ServiceProvider;
+            sayHello1_1 = serviceProvider3.GetService<ISayHello>();
+            sayHello1_1.Hi("M1_1 - Ada");
+            sayHello2_1 = serviceProvider3.GetService<ISayHello>();
+            sayHello2_1.Hi("M2_1 - Chan");
+            sayHello1_1 = null;
+            sayHello2_1 = null;
+            GC.Collect(2);
+            Thread.Sleep(1000);
+            sayHello3_1 = serviceProvider3.GetService<ISayHello>();
+            sayHello3_1.Hi("M3_1 - Ada Chan");
+
+            // (jasper) 箇要: 這裡用的是 scope2 的物件 ....
+            //
+            // 若將底下的程式碼註解起來(在 AddScoped 模式)，則 
+            // sayHello1, sayHello2 指向到 ConsoleMessage 會被釋放掉
+            sayHello3.Hi("M3 - Will");
 
             #endregion
 
@@ -167,6 +182,13 @@ namespace NETCoreDI
             serviceCollection.AddSingleton<ISayHello, SayHello>();
             serviceProvider1 = serviceCollection.BuildServiceProvider();
 
+            // -----------------------------------------------------
+            // (jasper)
+            // 1.. 因為是 AddSingleton, 所以, 即使是在不同的 scope, 仍然應該取得相同的物件
+            // 2.. sayHello1, sayHello2, sayHello1_1, sayHello2_1 雖然被設為 null,
+            //     但因為是 singleton, 仍然有 DI container 在使用中, 不會被釋放
+            // -----------------------------------------------------
+
             #region 使用單一 Singleton 
 
             serviceScope2 = serviceProvider1.CreateScope();
@@ -180,7 +202,7 @@ namespace NETCoreDI
             GC.Collect(2);
             Thread.Sleep(1000);
             sayHello3 = serviceProvider2.GetService<ISayHello>();
-            sayHello3.Hi("M9 - Will");
+            sayHello3.Hi("M3 - Will");
 
             serviceScope3 = serviceProvider1.CreateScope();
             serviceProvider3 = serviceScope3.ServiceProvider;
--- README.md
+++ README.md
@@ -4,6 +4,19 @@
 ## 問題:
 物件解構子執行的時間似乎不對.
 
+## 預期行為:  (不知描述是否正確?)
+
+### TestScoped(): 2 個 scope
+1.. 因為是 AddScoped, 建立不同的 scope 的狀況下, 會有不同的物件實體  
+2.. sayHello1, sayHello2 被設成 null, 因為還在 scope2 中, 所以不會被釋放.  
+3.. sayHello1_1, sayHello2_1 被設成 null, 因為還在 scope3 中, 所以不會被釋放.  
+    但此時因為前一個 scope 已經結束, 所以 sayHello1 (與 sayHello2 相同 instance), 應該要被解構掉.  
+    ==> 但實測結果是完全沒有被釋放, 不論 sayHello3.Hi("M3 - Will"); 是否有被註解掉.    
+### TestSingleton:
+1.. 因為是 AddSingleton, 所以, 即使是在不同的 scope, 仍然應該取得相同的物件.  
+2.. sayHello1, sayHello2, sayHello1_1, sayHello2_1 雖然被設為 null, 但因為是 singleton, 仍然有 DI container 在使用中, 不會被釋放.  
+
+
 ## 細節:
 1.. SolutionTransient, SolutionScoped, SolutionSingleton 都沒有看到物件釋放的訊息, 所以不確定是否有執行 ~SayHello() 這個解構子.
 
